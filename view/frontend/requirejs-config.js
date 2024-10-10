9cc3a8c801037e0317bc46ea76efcfd864748be8
Liam Toohey <liam.toohey@gene.co.uk>
2024-10-10 16:43:03 +0100

    Save payment if cart contains subscriptions

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,4 +1,11 @@
 var config = {
+    config: {
+        mixins: {
+            'PayPal_Braintree/js/view/payment/method-renderer/hosted-fields': {
+                'PayPal_Subscription/js/view/payment/method-renderer/hosted-fields-mixin': true
+            }
+        }
+    },
     map: {
         '*': {
             customerSubscriptionList: 'PayPal_Subscription/js/view/customer/subscription-list',
